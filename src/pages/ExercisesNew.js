d63643ea324d4e24a8502df279aac7e28b74ee28
Jose Libreros <jose.libreros@correounivalle.edu.co>
2021-01-07 23:34:02 -0500

    New form created to add new exercises

--- src/pages/ExercisesNew.js
+++ src/pages/ExercisesNew.js
@@ -1,20 +1,55 @@
 import React from 'react'
-import Exercises from './Exercises'
-
+import ExerciseForm from '../components/ExerciseForm'
+import Card from '../components/Card'
 class ExerciseNew extends React.Component{
 
-    
-    handleClick = () =>{
-        console.log(this.props)
+    state = {
+        form: {
+            title: '', 
+            description: '', 
+            img: '', 
+            leftColor: '', 
+            rightColor: ''
+        }
+    }
+
+    handleChange = e =>{
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    handleSubmit = e =>{
+        e.preventDefault()
+        console.log(this.state.form)
     }
 
     render(){
         return (
-            <button onClick={this.handleClick}>
-                Send
-            </button>
+            <div className="row">
+                <div className="col-sm">
+                    <Card {...this.state.form}/>
+
+                </div>
+                <div className="col-sm">
+                <ExerciseForm
+                onChange = {this.handleChange}
+                form = {this.state.form}
+                handleSubmit = {this.handleSubmit}
+            >
+
+            </ExerciseForm>
+                </div>
+            </div>
+            
+
+
         )
     }
 }
+
 
 export default ExerciseNew
